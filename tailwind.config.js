03fb6caa30ee159f46dc8d7a39f5fac7fbe6d550
salon-booking-prenotazione <koci.lorena@yahoo.com>
2026-01-30 23:17:00 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        serif: ["var(--font-serif)"],
+      },
+    },
   },
   plugins: [],
 };
